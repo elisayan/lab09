daea12694ceab1e34039cb46f9575d866c8f687f
Elisa Yan <elisa.yan@studio.unibo.it>
2023-11-23 22:23:55 +0100

    implemented Print button

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +18,62 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+    private final JFrame frame = new JFrame("Simple GUI");
+    private final Controller controller;
 
+    public SimpleGUI(Controller controller) {
+        this.controller = controller;
+
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        panel.add(textField, BorderLayout.NORTH);
+        final JTextArea textArea = new JTextArea();
+        panel.add(textArea, BorderLayout.CENTER);
+
+        final JPanel southPanel = new JPanel();
+        southPanel.setLayout(new BorderLayout());
+        panel.add(southPanel, BorderLayout.SOUTH);
+        final JButton print = new JButton("Print");
+        final JButton showHistory = new JButton("Show history");
+        southPanel.add(print, BorderLayout.LINE_START);
+        southPanel.add(showHistory, BorderLayout.LINE_END);
+        
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        print.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               SimpleGUI.this.controller.setNext(textField.getText());
+               SimpleGUI.this.controller.printString();
+            }
+            
+        });
+
+        showHistory.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+            
+        });
+    }
+
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI(new SimpleController()).display();
+    }
 }
